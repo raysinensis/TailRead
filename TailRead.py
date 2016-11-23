e397cc7e645381f122b8b3916cdfcd7419ae45df
raysinensis <raysinensis@gmail.com>
2016-11-23 00:21:21 -0500

    added rare scenarios

--- TailRead.py
+++ TailRead.py
@@ -12,9 +12,10 @@ files = glob.glob(path + '*.seq')
 standfile = "c:/bio/standard.txt"
 standdata = open(standfile, 'r')
 standseq = standdata.read()
+standtxtseq = standseq.replace("\n","")
 
 ##prepare output list of results
-finalresults = [['sequence', '3 end', 'tail sequence', 'tail length']]
+finalresults = [['name','sequence', '3 end', 'tail sequence', 'tail length']]
 
 ##for each single sequence
 for singlefile in files:
@@ -25,7 +26,7 @@ for singlefile in files:
 ##reverse complement if needed
 
 ##trim to designated sequence, around the 3' end
-	anchorseq = standseq[:19]
+	anchorseq = standtxtseq[:19]
 	txtseq = seq.replace("\n","")
 	trimmedseq = txtseq[(txtseq.find(anchorseq)):]
 
@@ -33,12 +34,14 @@ for singlefile in files:
 	x = 0
 	match = 0
 	mismatch = 0
-	while x < (len(trimmedseq)) and x < (len(standseq) and mismatch == 0:
+	while x < len(trimmedseq) and x < len(standseq) and mismatch == 0:
 		if trimmedseq[x] == standseq[x]:
 			match +=1
 		else:
 			mismatch +=1
 		x +=1
+	if mismatch == 1:
+		x -=1
 
 ##assess tail details
 	tossout = 0
@@ -51,8 +54,7 @@ for singlefile in files:
 	y = 1
 	z = 1
 	w = 1
-	if x == len(standseq):
-		y = 0
+	
 	while y < (len(trimmedseq)-x) and y < (len(standseq)-x) and tossout == 0:
 		if trimmedseq[x+y] == standseq[x+y]:
 			matchafter +=1
@@ -63,34 +65,40 @@ for singlefile in files:
 		if y == 3 and matchafter == 2:
 			tossout = 1
 			errmsg = 'mismatch'
-	while z < (len(trimmedseq)-x) and z < (len(standseq)-x) and tossout ==0:
+	while z < (len(trimmedseq)-x-1) and z < (len(standseq)-x) and tossout ==0:
 		if trimmedseq[x+z+1] == standseq[x+z]:
-			indelmatch +=1
+			inmatch +=1
 		z +=1
-		if z > 3 and (float(indelmatch)/(z-1)) > 0.75:
+		if z > 3 and (float(inmatch)/(z-1)) > 0.75:
 			tossout = 1
 			errmsg = 'insert'
-	while w < (len(trimmedseq)-x) and w < (len(standseq)-x) and tossout ==0:
+	while w < (len(trimmedseq)-x) and w < (len(standseq)-x-1) and tossout ==0:
 		if trimmedseq[x+w] == standseq[x+w+1]:
-			indelmatch +=1
+			delmatch +=1
 		w +=1
-		if w > 3 and (float(indelmatch)/(w-1)) > 0.75:
+		if w > 3 and (float(delmatch)/(w-1)) > 0.75:
 			tossout = 1
 			errmsg = 'deletion'
+	if x == len(standseq):
+		y = 0
 	if tossout == 0 and y > x:
 		tossout = 1
 		errmsg = 'match no confidence'
 	if tossout == 0:
-		taillength = y
+		if len(trimmedseq) <= len(standseq):
+			taillength = y
+		else:
+			taillength = len(trimmedseq) - x
 		tailseq = trimmedseq[x:]
 	else:
 		taillength = 'error'
 		tailseq = errmsg
 
-	finalresults.append([trimmedseq,x,taillength,tailseq])
+	finalresults.append([singlefile,trimmedseq,x,tailseq,taillength])
 
-outputfile = path + output.csv
+outputfile = path + 'output.csv'
 outputwriter = open(outputfile, 'w')
 csv.writer(outputwriter).writerows(finalresults)
+outputwriter.close()
 	
 	
